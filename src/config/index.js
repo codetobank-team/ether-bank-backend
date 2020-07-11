a45b34f8b25aa7232221d006d857f45572cde981
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-07-11 10:14:06 +0100

    Added logging

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   PORT: process.env.PORT,
   APP_NAME: process.env.APP_NAME || 'Ether Bank',
+  NODE_ENV: process.env.NODE_ENV,
 };
--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const app = require('./expressApp');
 const { PORT, APP_NAME } = require('./config');
+const { logger } = require('./utils');
 
 const horizontalLine = '\n--------------------------------------------------\n';
+
+const serverLogger = logger(module);
+serverLogger.log('info', 'Server started');
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
