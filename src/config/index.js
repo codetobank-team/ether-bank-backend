fdc65dee9c1aa87ec66df4f79bf83631980dc6c1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-07-13 17:38:23 +0100

    Set up the mongo db

--- src/config/index.js
+++ src/config/index.js
@@ -2,4 +2,6 @@ module.exports = {
   PORT: process.env.PORT,
   APP_NAME: process.env.APP_NAME || 'Ether Bank',
   NODE_ENV: process.env.NODE_ENV,
+  MONGO_HOST: process.env.MONGO_HOST,
+  MONGO_PORT: process.env.MONGO_PORT,
 };
