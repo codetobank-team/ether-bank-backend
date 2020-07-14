b1f35c0c4f4fb410570ffb9cf8c76bd7ec8f7cb2
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-07-14 02:02:59 +0100

    Fixed logged in middleware

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,10 +2,12 @@ const logger = require('./logger');
 const bcryptUtils = require('./bcrypt');
 const jwtUtils = require('./jwt');
 const responseObject = require('./responseObject');
+const isLoggedIn = require('./isLoggedIn');
 
 module.exports = {
   logger,
   bcryptUtils,
   jwtUtils,
   responseObject,
+  isLoggedIn,
 };
--- src/utils/isLoggedIn.js
+++ src/utils/isLoggedIn.js
@@ -1,13 +1,17 @@
 const {
-  responseObject,
-  jwtUtils: { verify },
-} = require('./index');
+  verify,
+} = require('./jwt');
+const responseObject = require('./responseObject');
 const { getAsync } = require('../redis');
 
 // eslint-disable-next-line consistent-return
 module.exports = async (req, res, next) => {
   try {
-    const token = req.get('Authorization').split(' ')[1];
+    const authHeader = req.get('Authorization');
+
+    if (!authHeader) return responseObject(res, 401, 'You must be logged in to use this endpoint', 'error');
+
+    const token = authHeader.split(' ')[1];
     const { id } = req.body;
 
     if (!token) return responseObject(res, 401, 'You must be logged in to use this endpoint', 'error');
