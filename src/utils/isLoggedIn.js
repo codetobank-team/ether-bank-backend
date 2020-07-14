14a1e8a15645df024aef2605b0553deff2080599
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-07-14 19:47:46 +0100

    Is logged in does not check req body

--- src/utils/isLoggedIn.js
+++ src/utils/isLoggedIn.js
@@ -8,23 +8,20 @@ const { getAsync } = require('../redis');
 module.exports = async (req, res, next) => {
   try {
     const authHeader = req.get('Authorization');
-
     if (!authHeader) return responseObject(res, 401, 'You must be logged in to use this endpoint', 'error');
 
     const token = authHeader.split(' ')[1];
-    const { id } = req.body;
-
     if (!token) return responseObject(res, 401, 'You must be logged in to use this endpoint', 'error');
+
+    const payload = verify(token);
+    if (!payload) return responseObject(res, 401, 'You must be logged in to use this endpoint', 'error');
+    const { id } = payload;
 
     const cachedToken = await getAsync(`${id}-token`);
 
     if (!cachedToken || token !== cachedToken) return responseObject(res, 401, 'You must be logged in to use this endpoint', 'error');
 
-    const payload = verify(token);
-
-    if (!payload) return responseObject(res, 401, 'You must be logged in to use this endpoint', 'error');
-
-    req.userId = payload.id;
+    req.userId = id;
     next();
   } catch (err) {
     return responseObject(res, 500, `Eror: ${err.message}`, 'error');
