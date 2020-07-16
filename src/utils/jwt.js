5341f4bd7e2fc3d735f39d928ec94714a01d1e0e
Ndubuisi Samuel <samniwebdev@gmail.com>
2020-07-16 02:11:37 +0100

    fix: extend jwt ttl

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -2,6 +2,6 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../config');
 
 module.exports = {
-  sign: (payload) => jwt.sign(payload, JWT_SECRET, { expiresIn: 60 * 30 }),
+  sign: (payload) => jwt.sign(payload, JWT_SECRET, { expiresIn: 60 * 60 * 24  }),
   verify: (token) => jwt.verify(token, JWT_SECRET),
 };
